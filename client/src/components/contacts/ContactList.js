723f36182cfc2e5c60dc1e15f95f5cb298739029
Kinga Humeniuk <kinghum669@student.polsl.pl>
2019-05-16 22:45:05 +0200

    kinga - small refaacgor

--- client/src/components/contacts/ContactList.js
+++ client/src/components/contacts/ContactList.js
@@ -4,8 +4,6 @@ import SocketContext from '../../config/socketContext'
 
 import './ContactList.css'
 
-
-
 class ContactList extends Component {
 
     state = {
@@ -28,39 +26,15 @@ class ContactList extends Component {
             return <ContactItem nick={user} key={index} onClick={() => this.onItemClicked(user)}/>
         })
         return (
-        /*
-            <div>
+            <div class="card">
+                <div class="container">
                 <h1 className="list-title"> Contact List</h1>
-                {contactItems}
+                    {contactItems}
+                </div>
             </div>
-            */
-           /*
-           <div class="card" >
-
-           <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap"/>
-         
-           <div class="card-body">
-         
-             <h4 class="card-title"><a>Card title</a></h4>
-             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             <a href="#" class="btn btn-primary">Button</a>
-         
-           </div>
-         
-         </div>
-         */
-
-        <div class="card">
-  <div class="container">
-  <h1 className="list-title"> Contact List</h1>
-                {contactItems}
-    <p>{contactItems}</p> 
-  </div>
-</div>
         )
     }
 }
-
 
 const ContactListWithSocket = props => (
     <SocketContext.Consumer>
