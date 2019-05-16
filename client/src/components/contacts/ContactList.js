38fdc107093e465260c1bc7ae46c7fa5f657e112
Kinga Humeniuk <kinghum669@student.polsl.pl>
2019-05-16 22:39:03 +0200

    kinga - main view

--- client/src/components/contacts/ContactList.js
+++ client/src/components/contacts/ContactList.js
@@ -4,6 +4,8 @@ import SocketContext from '../../config/socketContext'
 
 import './ContactList.css'
 
+
+
 class ContactList extends Component {
 
     state = {
@@ -26,10 +28,35 @@ class ContactList extends Component {
             return <ContactItem nick={user} key={index} onClick={() => this.onItemClicked(user)}/>
         })
         return (
+        /*
             <div>
                 <h1 className="list-title"> Contact List</h1>
                 {contactItems}
             </div>
+            */
+           /*
+           <div class="card" >
+
+           <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap"/>
+         
+           <div class="card-body">
+         
+             <h4 class="card-title"><a>Card title</a></h4>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <a href="#" class="btn btn-primary">Button</a>
+         
+           </div>
+         
+         </div>
+         */
+
+        <div class="card">
+  <div class="container">
+  <h1 className="list-title"> Contact List</h1>
+                {contactItems}
+    <p>{contactItems}</p> 
+  </div>
+</div>
         )
     }
 }
