269a27ea9cdd2dc0cd35608f6126f4cf264acf0a
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2019-05-16 20:40:04 +0200

    Added redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'typeface-roboto'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/app/App'
 import * as serviceWorker from './serviceWorker'
+import chatReducer from './reducers/chatReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(chatReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
