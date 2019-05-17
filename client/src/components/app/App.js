b088eb04f36120ca1ebec23f1004dc1fe4f2dc08
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2019-05-17 22:33:56 +0200

    Refactor in App.js

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,52 +1,53 @@
 import React, { Component } from 'react'
-
 import * as io from 'socket.io-client'
-import './App.css'
 import SocketContext from '../../config/socketContext'
 import ChatBox from '../chatbox/ChatBox'
 import ContactList from '../contacts/ContactList'
 import WelcomeDialog from '../widgets/WelcomeDialog'
 import Swal from 'sweetalert2'
+import { connect } from 'react-redux'
+
+import './App.css'
 
 class App extends Component {
 
   state = {
-    userNick: '',
     socket: null,
     userToChat: '',
   }
 
-  handleEnteredNick = (nick) => {
-    // if user provides his nick, app is connected with server via socket
-    this.setState({
-      userNick: nick,
-      socket: io(process.env.REACT_APP_SERVER_URL, { query: `nick=${nick}` })
-    }, () => {
-      this.state.socket.on(process.env.REACT_APP_NOTIFY_USER_EVENT, ({ notifierUserNick, roomName }) => {
-        
-        Swal.fire({
-          title: 'Want to chat?',
-          text: 'User ' + notifierUserNick + ' wants to chat with You on room ' + roomName + ' !',
-          type: 'success',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Chat'
-        }).then((result) => {
-          if (result.value) {
-            Swal.fire(
-              'You can chat now!',
-              'Good luck :)',
-              'success'
-            )
-          }
-        })
-
+  componentWillReceiveProps({ userNick }) {
+    console.log(userNick)
+    if (userNick) {
+      this.setState({
+        socket: io(process.env.REACT_APP_SERVER_URL, { query: `nick=${userNick}` })
+      }, () => {
+        this.state.socket.on(process.env.REACT_APP_NOTIFY_USER_EVENT, ({ notifierUserNick, roomName }) => this.showNotification(notifierUserNick, roomName))
       })
+    }
+  }
+
+  showNotification = (notifierUserNick, roomName) => {
+    Swal.fire({
+      title: 'Want to chat?',
+      text: `User ${notifierUserNick} wants to chat with You on room ${roomName} !`,
+      type: 'success',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Chat'
+    }).then(result => {
+      if (result.value) {
+        Swal.fire(
+          'You can chat now!',
+          'Good luck :)',
+          'success'
+        )
+      }
     })
   }
 
-  selectUserToChat = (userToChat) => {
+  selectUserToChat = userToChat => {
     if (this.state.userNick === userToChat) {
       Swal.fire({
         type: 'error',
@@ -65,7 +66,7 @@ class App extends Component {
       contactList = <ContactList userToChat={this.selectUserToChat} />
     }
     if (this.state.userToChat) {
-      messageBox = <ChatBox userToChat={this.state.userToChat}/>
+      messageBox = <ChatBox userToChat={this.state.userToChat} />
     }
 
     return (
@@ -73,10 +74,16 @@ class App extends Component {
       <SocketContext.Provider value={this.state.socket}>
         {messageBox}
         {contactList}
-        <WelcomeDialog nick={this.handleEnteredNick} />
+        <WelcomeDialog />
       </SocketContext.Provider>
     )
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    userNick: state.userNick
+  }
+}
+
+export default connect(mapStateToProps)(App)
